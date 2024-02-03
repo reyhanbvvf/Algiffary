cb80fc4fafc3f841a32dbe2c0b2f4359d54d98a2
dedovis <dedovis@gmail.com>
2024-02-04 02:45:53 +0800

    fix name sidebar

--- resources/views/master/back.blade.php
+++ resources/views/master/back.blade.php
@@ -209,7 +209,7 @@
           class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">{{Auth::user()->name}}</a>
         </div>
       </div>
 
