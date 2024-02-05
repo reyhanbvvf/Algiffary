17eee0611ffa1da3245dacf8d8af618426f8e12e
ahmadridhosetiawan11 <ahmadridhosetiawan11@gmail.com>
2024-02-05 12:11:28 +0800

    ridhojaa

--- resources/views/master/sidebar/superadmin.blade.php
+++ resources/views/master/sidebar/superadmin.blade.php
@@ -30,7 +30,7 @@
         </a>
       </li>
 
-      <li class="nav-item">
+      {{-- <li class="nav-item">
         <a href="#" class="nav-link">
           <i class="nav-icon fas fa-copy"></i>
           <p>
@@ -621,7 +621,7 @@
         <a href="#" class="nav-link">
           <i class="nav-icon far fa-circle text-info"></i>
           <p>Informational</p>
-        </a>
+        </a> --}}
       </li>
     </ul>
   </nav>
