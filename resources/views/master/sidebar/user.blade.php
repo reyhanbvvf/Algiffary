826939f6c7f00cce23c6c9ebda378077908047da
reyhanbvvf <zeusgiffary@gmail.com>
2024-03-11 15:06:55 +0800

    tagihan

--- resources/views/master/sidebar/user.blade.php
+++ resources/views/master/sidebar/user.blade.php
@@ -27,16 +27,16 @@
                 </p>
             </a>
         </li>
-      {{-- <li class="nav-item">
+      <li class="nav-item">
         <a href="pages/widgets.html" class="nav-link">
-          <i class="nav-icon fas fa-th"></i>
+            <i class="nav-icon fas fa-file-invoice"></i>
           <p>
-            Widgets
-            <span class="right badge badge-danger">New</span>
+            Invoice
+            <span class="right badge badge-danger">Notif</span>
           </p>
         </a>
       </li>
-      <li class="nav-item">
+      {{-- <li class="nav-item">
         <a href="#" class="nav-link">
           <i class="nav-icon fas fa-copy"></i>
           <p>
