ba8f81861d071e7b34b2471345d5ed37e2441f5a
reyhanbvvf <zeusgiffary@gmail.com>
2024-10-02 12:55:24 +0800

    map

--- resources/views/back/superadmin/map/index.blade.php
+++ resources/views/back/superadmin/map/index.blade.php
@@ -59,101 +59,98 @@ Peta Perusahaan
 @endsection
 
 @push('script')
-    <script>
-        var map = L.map('map').setView([-3.3571046, 115.0606351], 10);
+<script>
+    // Inisialisasi peta
+    var map = L.map('map').setView([-3.3571046, 115.0606351], 10);
 
-        // Define base layers
-        var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
+    // Lapisan dasar peta
+    var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 
-        var gmap = L.tileLayer('http://{s}.google.com/vt?lyrs=m,h&x={x}&y={y}&z={z}', {
-            maxZoom: 20,
-            subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-        });
+    var gmap = L.tileLayer('http://{s}.google.com/vt?lyrs=m,h&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    });
 
-        var gsmap = L.tileLayer('http://{s}.google.com/vt?lyrs=s,h&x={x}&y={y}&z={z}', {
-            maxZoom: 20,
-            subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-        });
+    var gsmap = L.tileLayer('http://{s}.google.com/vt?lyrs=s,h&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    });
 
-        // Define overlay layer
-        var category2Layer = L.layerGroup();
+    // Lapisan overlay
+    var category2Layer = L.layerGroup().addTo(map);
 
-        // Add overlay layer to overlayMaps object
-        var overlayMaps = {
-            "Perusahaan": category2Layer,
-        };
+    // Kontrol lapisan
+    var layerControl = L.control.layers({
+        "OpenStreetMap": osm,
+        "Google Maps": gmap,
+        "Google Satellite Maps": gsmap
+    }, {
+        "Perusahaan": category2Layer
+    }, {
+        collapsed: false
+    }).addTo(map);
 
-        // Add layers control to the map with all layers selected by default
-        var layerControl = L.control.layers({
-            "OpenStreetMap": osm,
-            "Google Maps": gmap,
-            "Google Satellite Maps": gsmap
-        }, overlayMaps, {
-            collapsed: false // Show the layers control expanded by default
-        }).addTo(map);
+    // Menambahkan marker dari data kebakaran
+    @foreach($data as $d)
+        // Konten popup
+        var popupContent = `
+            <div class="custom-popup">
+                <b>Perusahaan</b><br>
+                <b>Nama Perusahaan :</b><br>${{ $d->nama ? $d->nama : '-' }}<br>
+                <b>Alamat :</b><br>{{ $d->alamat }}<br>
+                <b>Foto :</b><br>
+                @if ($d->foto)
+                    <div style="text-align: center;">
+                        <img src="{{ asset('storage/user/' . $d->foto) }}" class="img-fluid" alt="Foto Perusahaan" style="max-width: 200px; max-height: 200px; margin: 0 auto;">
+                    </div><br>
+                @else
+                    No photos available<br>
+                @endif
+            </div>
+        `;
 
-        // Add the overlay layer to the map
-        category2Layer.addTo(map);
-
-        // Iterate over Kebakaran data and add markers to category2Layer
-        @foreach($data as $d)
-
-        // Initialize popup content
-        var popupContent = `<div class="custom-popup">`;
-
-        popupContent += `<b>Perusahaan</b><br>
-                        <b>Nama Perusahaan :</b><br>{{ $d->nama ? $d->nama : '-' }}<br>
-                        <br>
-                        <b>Alamat :</b><br>{{ $d->alamat }}<br>
-                        <br>
-                        <b>Foto :</b><br>`;
-
-        // Check if photos are available
-        @if ($d->foto)
-            popupContent += `<div style="text-align: center;"><img src="{{ asset('storage/user/' . $d->foto) }}" class="img-fluid" alt="Foto Perusahaan" style="max-width: 200px; max-height: 200px; margin: 0 auto;"></div><br>`; // Center the image
-        @else
-            // If no photos available, display a message
-            popupContent += `No photos available<br>`;
-        @endif
-
-        // Add a link to view details
-        popupContent += `<br>`
-                        ;
-
-        popupContent += `</div>`;
-
-        // Create marker and bind popup
+        // Membuat marker
         var marker = L.marker([{{ $d->latitude }}, {{ $d->longitude }}]).addTo(category2Layer);
-        marker.bindPopup(popupContent, { maxWidth: 400, maxHeight: 400 }); // Set the maximum width and height of the popup container
-        @endforeach
-    </script>
+        marker.bindPopup(popupContent, { maxWidth: 400, maxHeight: 400 });
 
+        ($data as $d)
+        if ({{ $d->latitude }} && {{ $d->longitude }}) {
+            var marker = L.marker([{{ $d->latitude }}, {{ $d->longitude }}]).addTo(category2Layer);
+            marker.bindPopup(popupContent, { maxWidth: 400, maxHeight: 400 });
+        }
+    @endforeach
+</script>
 
-    <script>
-        $(function () {
+<script>
+    $(function () {
+        // DataTable untuk tabel
         $("#example1").DataTable({
-            "responsive": true, "lengthChange": false, "autoWidth": false,
-            "buttons": [""]
+            responsive: true, 
+            lengthChange: false, 
+            autoWidth: false,
+            buttons: [""]
         }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-        $('#example2').DataTable({
-            "paging": true,
-            "lengthChange": false,
-            "searching": false,
-            "ordering": true,
-            "info": true,
-            "autoWidth": false,
-            "responsive": true,
-        });
-        });
-    </script>
 
-    <script>
-        $('.delete').on('click', function(){
-        var link = $(this).data('link');
-        $('#formDelete').attr('action',link)
+        $('#example2').DataTable({
+            paging: true,
+            lengthChange: false,
+            searching: false,
+            ordering: true,
+            info: true,
+            autoWidth: false,
+            responsive: true,
         });
-    </script>
+    });
+
+    // Fungsi untuk menghapus data
+    $('.delete').on('click', function() {
+        var link = $(this).data('link');
+        $('#formDelete').attr('action', link);
+    });
+</script>
 @endpush
+
+
